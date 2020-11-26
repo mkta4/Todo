bc60438f5dbbaa4b8134f7c1c304d4e1ca3b404f
Stefan Novikov <darksarksn@gmail.com>
2020-11-26 15:11:30 +0300

    init index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Todo1</li>
+            <li>Todo2</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+   return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+   return <input placeholder="search"/>;
+};
+
+const App = () => {
+    return (
+    <div>
+        <AppHeader />
+        <SearchPanel />
+        <TodoList />
+    </div>
+    );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
