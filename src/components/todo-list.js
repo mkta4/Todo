a40a75661ec907a5b1f79badf720e73f20cf1750
Stefan Novikov <darksarksn@gmail.com>
2020-11-26 16:16:40 +0300

    add project structure

---
+++ src/components/todo-list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Todo1</li>
+            <li>Todo2</li>
+        </ul>
+    );
+};
+
+export default TodoList;
