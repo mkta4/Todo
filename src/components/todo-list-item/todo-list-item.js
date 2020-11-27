6d369c6b62d6cf97c343cc0775488ef8e4390acf
Stefan Novikov <darksarksn@gmail.com>
2020-11-27 14:43:32 +0300

    feat: add event done and important

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,33 +1,63 @@
 import React from 'react';
-
 import './todo-list-item.css';
 
-const TodoListItem = ( {label, important = false} ) => {
+export default class TodoListItem extends React.Component {
+
+  state = {
+    done: false,
+    important: false,
+  };
+
+  onLabelClick = () => {
+   this.setState(( {done} ) => {
+     return {
+       done: !done
+     };
+   });
+  };
+
+  onMarkImportant = () => {
+    this.setState(( {important} ) => {
+      return {
+        important: !important
+      };
+    });
+  };
+
+  render() {
+   const  { label,  } = this.props;
+   const { done, important }  = this.state;
+
+   let classNames = 'todo-list-item';
+   if (done) {
+     classNames += ' done';
+   } 
+
+   if (important) {
+    classNames += ' important';
+   }
    
-    const spanStyle = {
-        color: important ? 'steelblue' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    };
-    
     return (
-        <span className="todo-list-item">
+        <span className={classNames}>
           <span
             className="todo-list-item-label"
-            style={spanStyle}>
+            onClick={ this.onLabelClick } >
             {label}
           </span>
     
           <button type="button"
-                  className="btn btn-outline-success btn-sm float-right">
+                  className="btn btn-outline-success btn-sm float-right"
+                  onClick={ this.onMarkImportant }>
             <i className="fa fa-exclamation" />
           </button>
     
           <button type="button"
-                  className="btn btn-outline-danger btn-sm float-right">
+                  className="btn btn-outline-danger btn-sm float-right"
+                  >
             <i className="fa fa-trash-o" />
           </button>
         </span>
       );
 };
+};
 
-export default TodoListItem;
